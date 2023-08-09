136edc75623054fd81b158378ea92566d666f924
Mukesh <mukeshmahato2618@gmail.com>
2023-08-09 15:55:39 +0545

    updating key value store

--- Key_Value Store/main.go
+++ Key_Value Store/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"sync"
 )
 
 type Storer[K comparable, V any] interface {
 	Put(K, V) error
-	Get(K) ([]byte, error)
-	Update(K, []byte) error
+	Get(K) (V, error)
+	Update(K, V) error
 	Delete(K) (V, error)
 }
 
@@ -57,38 +58,60 @@ func (s *KVStore[K, V]) Get(key K) (V, error) {
 	return value, nil
 }
 
+func (s *KVStore[K, V]) Delete(key K) (V, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	value, ok := s.data[key]
+	if !ok {
+		return value, fmt.Errorf("the key (%v) doesn't exists", key)
+	}
+
+	delete(s.data, key)
+	return value, nil
+}
+
 func NewKVStore[K comparable, V any]() *KVStore[K, V] {
 	return &KVStore[K, V]{
 		data: make(map[K]V),
 	}
 }
 
-func StoreThings(s Storer[string, int]) error {
-	return s.Put("foo", 1)
+type User struct {
+	ID        int
+	FirstName string
+	Age       int
+	Gender    string
+}
+
+type Server struct {
+	Storage    Storer[int, *User]
+	ListenAddr string
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		Storage:    NewKVStore[int, *User](),
+		ListenAddr: listenAddr,
+	}
+}
+
+func (s *Server) handlePut(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("foo"))
+}
+
+func (s *Server) Start() {
+	fmt.Printf("server is running on port %s", s.ListenAddr)
+
+	http.HandleFunc("/put", s.handlePut)
+
+	log.Fatal(http.ListenAndServe(s.ListenAddr, nil))
 }
 
 func main() {
-	store := NewKVStore[string, string]()
+	s := NewServer(":3000")
 
-	if err := store.Put("foo", "bar"); err != nil {
-		log.Fatal(err)
-	}
+	s.Start()
 
-	value, err := store.Get("foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(value)
-
-	if err := store.Put("foo", "oof"); err != nil {
-		log.Fatal(err)
-	}
-
-	value, err = store.Get("foo")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(value)
 }
