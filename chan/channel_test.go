0bd52cd8d219ed8d4e1686b2208e4e38dd118ef7
Mukesh Mahato <124282280+mukeshmahato18@users.noreply.github.com>
2023-08-15 19:35:01 +0545

    Update channel_test.go
    adding test for channel

--- chan/channel_test.go
+++ chan/channel_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAddUsers(t *testing.T) {
+	server := NewServer()
+
+	for i := 0; i < 10; i++ {
+		// go func(i int) {
+		server.userch <- fmt.Sprintf("user_%d", i)
+		// server.addUser(fmt.Sprintf("user_%d", i))
+		// }(i)
+	}
+	fmt.Println("the loop is done!")
+}
