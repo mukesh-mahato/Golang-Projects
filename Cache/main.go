916efc8498a1bc693b6d661c76988a0041377317
Mukesh Mahato <124282280+mukeshmahato18@users.noreply.github.com>
2023-08-21 16:17:09 +0545

    Updating main.go

--- Cache/main.go
+++ Cache/main.go
@@ -1,11 +1,24 @@
 package main
 
-import cache "github.com/mukeshmahato18/cache/Cache"
+import (
+	"time"
+	"net"
+	"log"
+	cache "github.com/mukeshmahato18/cache/Cache"
+	)
 
 func main() {
 	opts := ServerOpts{
 		ListenAddr: ":3000",
 		IsLeader:   true,
+	}
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		conn, err := net.Dial("tcp", ":3000")
+		if err!= nil {
+			log.Fatal(err)
+		}
 	}
 
 	server := NewServer(opts, cache.NewCacher())
