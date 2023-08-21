10b995f0da74f9b227f2f020592d0063aa20f1d9
Mukesh Mahato <124282280+mukeshmahato18@users.noreply.github.com>
2023-08-21 15:57:40 +0545

    message added

--- Cache/command.go
+++ Cache/command.go
@@ -1,11 +1,20 @@
 package main
 
+import "time"
+
 type Commnd string
 
 const (
   CMDSet Command = "SET"
   CMDGet Command = "SET"
 )
+
+type Message struct {
+  cmd Command
+  key []byte
+  value []byte
+  TTL time.Duration
+}
 
 func parseCommand(raw []byte) (*Message, error) {
   
